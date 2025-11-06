cd2cddf65da05aba93a69fb6a6b400f1c0f3067b
Hossein <hossein.aghili.2658@gmail.com>
2025-11-06 21:52:03 +0330

    create fuction update user

--- api/Controllers/user.controller.js
+++ api/Controllers/user.controller.js
@@ -32,3 +32,28 @@ export const getOne = catchAsync(async (req, res, next) => {
         message: 'user successfully'
     })
 })
+export const update = catchAsync(async (req, res, next) => {
+    const { id } = req.params
+    if (req.role !== 'admin' && req.userId !== id) {
+        return next(new HandleERROR('you dont have permission'), 401)
+    }
+    let user
+    if (req.role === 'admin') {
+        const { password = null, ...others } = req.body
+        user = await User.findByIdAndUpdate(id, others, {
+            new: true,
+            runValidators: true
+        })
+    } else {
+        const { password = null, role = null, ...others } = req.body
+        user = await User.findByIdAndUpdate(id, others, {
+            new: true,
+            runValidators: true
+        })
+    }
+    return res.status(200).json(({
+        success: true,
+        data: user,
+        message: 'update successfully'
+    }))
+})
