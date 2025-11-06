5cad91049f822050989485a961847b6efd011f58
Hossein <hossein.aghili.2658@gmail.com>
2025-11-06 21:17:29 +0330

    import route auth and error routing

--- api/app.js
+++ api/app.js
@@ -3,6 +3,8 @@ import path from 'path'
 import { fileURLToPath } from 'url'
 import cors from 'cors'
 import morgan from 'morgan'
+import authRouter from './Routes/auth.route.js'
+import { HandleERROR } from 'vanta-api'
 const __filename =fileURLToPath(import.meta.url)
 export const __dirname = path.dirname(__filename)
 
@@ -10,6 +12,10 @@ const app = express()
 app.use(express.json())
 app.use(cors())
 app.use(morgan('dev'))
+app.use('/api/user',authRouter)
 
+app.use((req, res, next) => {
+    next(new HandleERROR('Route not Found', 404))
+})
 
 export default app 
