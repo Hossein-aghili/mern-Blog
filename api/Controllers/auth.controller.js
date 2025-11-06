c2b33ef57cde18bce27d9cb8fe4bac0292567433
Hossein <hossein.aghili.2658@gmail.com>
2025-11-06 17:39:20 +0330

    create function register controller

---
+++ api/Controllers/auth.controller.js
@@ -0,0 +1,22 @@
+import { catchAsync, HandleERROR } from "vanta-api";
+import User from "../Models/user.model.js";
+import jwt from "jsonwebtoken";
+import bcryptjs from "bcryptjs";
+const register = catchAsync(async (req, res, next) => {
+    const { password = '', role = '', ...others } = req.body
+    if (!username || !password || !role) {
+        return next(new HandleERROR('username or password is required'))
+    }
+    const passwordRegex = /^(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+    if (!passwordRegex.test(password)) {
+        return next(new HandleERROR("Password must be at least 8 characters long, include an uppercase letter, a number, and a special character."))
+    }
+    const hashPassword = await bcryptjs.hash(password, 10)
+    const user = await User.create({ password: hashPassword, ...others })
+
+    return res.status(200).json({
+        success: true,
+        data: user,
+        message: 'با موفقیت ساخته شد'
+    })
+})
