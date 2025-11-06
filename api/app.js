6a649f9e289b14972cf1d259548506a0d049577f
Hossein <hossein.aghili.2658@gmail.com>
2025-11-06 09:18:38 +0330

    Calling filename

---
+++ api/app.js
@@ -0,0 +1,6 @@
+import express from 'express'
+import path from 'path'
+import { fileURLToPath } from 'url'
+
+const __filename =fileURLToPath(import.meta.url)
+export const __dirname = path.dirname(__filename)
