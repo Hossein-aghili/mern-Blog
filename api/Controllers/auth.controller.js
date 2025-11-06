1b991cc6620631d743436be99ccf3be3eded6996
Hossein <hossein.aghili.2658@gmail.com>
2025-11-06 17:59:53 +0330

    create function login controller

--- api/Controllers/auth.controller.js
+++ api/Controllers/auth.controller.js
@@ -5,7 +5,7 @@ import bcryptjs from "bcryptjs";
 const register = catchAsync(async (req, res, next) => {
     const { password = '', role = '', ...others } = req.body
     if (!username || !password || !role) {
-        return next(new HandleERROR('username or password is required'))
+        return next(new HandleERROR('username or password is required'), 400)
     }
     const passwordRegex = /^(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
     if (!passwordRegex.test(password)) {
@@ -17,6 +17,32 @@ const register = catchAsync(async (req, res, next) => {
     return res.status(200).json({
         success: true,
         data: user,
-        message: 'با موفقیت ساخته شد'
+        message: 'register successfully'
+    })
+})
+const login = catchAsync(async (req, res, next) => {
+    const { password = null, username = null } = req.body
+    if (!password || !username) {
+        return next(new HandleERROR('username or password is required'), 400)
+    }
+    const user = await User.findOne({ username })
+    if (!user) {
+        return next(new HandleERROR('user not fund'))
+    }
+    const checkPassword = bcryptjs.compareSync(password, user.password)
+    if (!checkPassword) {
+        return next(new HandleERROR('Invalid Password'), 400)
+    }
+    const token = jwt.sign({ id: user._id, role: user.role }, process.env.JWT_SECRET)
+
+    return res.status(200).json({
+        success:true,
+        data:{
+            id:user._id,
+            role:user.role,
+            username:user.username,
+            email:user.email
+        },
+        message:'login successfully'
     })
 })
