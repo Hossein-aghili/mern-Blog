07c16dde376187e9c46a6e2db30c8056446dc0ef
Hossein <hossein.aghili.2658@gmail.com>
2025-11-08 20:51:06 +0330

    create route category

---
+++ api/Routes/category.route.js
@@ -0,0 +1,7 @@
+import express from 'express'
+import { create, getAll, getOne, remove, update } from '../Controllers/category.controller.js'
+import { isAdmin } from '../Middlewares/isAdmin.js'
+const categoryRouter = express.Router()
+categoryRouter.route('/').get(getAll).post( create)
+categoryRouter.route('/:id').get(getOne).patch(isAdmin, update).delete(isAdmin, remove)
+export default categoryRouter
