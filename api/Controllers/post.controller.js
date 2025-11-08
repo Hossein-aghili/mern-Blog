633677384b35b9584729737ec56822d7199876f5
Hossein <hossein.aghili.2658@gmail.com>
2025-11-08 21:19:44 +0330

    create function update controller post

--- api/Controllers/post.controller.js
+++ api/Controllers/post.controller.js
@@ -1,5 +1,6 @@
 import ApiFeatures, { catchAsync, HandleERROR } from "vanta-api";
 import Post from "../Models/post.model.js";
+import { update } from "./user.controller.js";
 
 export const create = catchAsync(async (re, res, next) => {
     const post = await Post.create(req.body)
@@ -9,29 +10,43 @@ export const create = catchAsync(async (re, res, next) => {
         message: 'create post successfully'
     })
 })
-export const getAll = catchAsync(async(req,res,next)=>{
-    const features = new ApiFeatures(Post,req.query,req.role)
-    .filter()
-    .sort()
-    .paginate()
-    .limitFields()
-    .populate()
+export const getAll = catchAsync(async (req, res, next) => {
+    const features = new ApiFeatures(Post, req.query, req.role)
+        .filter()
+        .sort()
+        .paginate()
+        .limitFields()
+        .populate()
     const posts = await features.execute()
     const count = await Post.countDocuments(features.queryFilter)
     return res.status(200).json({
-        success:true,
-        data:posts,
+        success: true,
+        data: posts,
         count
     })
 })
-export const getOne = catchAsync(async(req,res,next)=>{
-    const {id} =req.params
+export const getOne = catchAsync(async (req, res, next) => {
+    const { id } = req.params
     const post = await Post.findById(id)
-    if(!post){
-        return next(new HandleERROR('post not found'),404)
+    if (!post) {
+        return next(new HandleERROR('post not found'), 404)
     }
     return res.status(200).json({
-        success:true,
-        data:post
+        success: true,
+        data: post
+    })
+})
+export const update = catchAsync(async (req, res, next) => {
+    const { id } = req.params
+    const post = await Post.findByIdAndUpdate(id, req.body, {
+        new: true,
+        runValidators: true
+    })
+    if (!post) {
+        return next(new HandleERROR('post not found'), 404)
+    }
+    return res.status(200).json({
+        success: true,
+        data: post
     })
 })
