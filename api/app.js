c94e3de73587a05afa6b642c182f9406dee2ff29
Hossein <hossein.aghili.2658@gmail.com>
2025-11-08 21:31:54 +0330

    create route middlewres

--- api/app.js
+++ api/app.js
@@ -5,6 +5,10 @@ import cors from 'cors'
 import morgan from 'morgan'
 import authRouter from './Routes/auth.route.js'
 import { HandleERROR } from 'vanta-api'
+import userRouter from './Routes/user.route.js'
+import categoryRouter from './Routes/category.route.js'
+import postRouter from './Routes/post.route.js'
+
 const __filename =fileURLToPath(import.meta.url)
 export const __dirname = path.dirname(__filename)
 
@@ -12,7 +16,10 @@ const app = express()
 app.use(express.json())
 app.use(cors())
 app.use(morgan('dev'))
-app.use('/api/user',authRouter)
+app.use('/api/auth', authRouter)
+app.use('/api/user', userRouter)
+app.use('/api/category', categoryRouter)
+app.use('/api/post', postRouter)
 
 app.use((req, res, next) => {
     next(new HandleERROR('Route not Found', 404))
