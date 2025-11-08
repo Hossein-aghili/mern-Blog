845f606b5ce0cabb924179488829e93c19a29e6f
Hossein <hossein.aghili.2658@gmail.com>
2025-11-08 21:12:44 +0330

    create function getAll controller post

--- api/Controllers/post.controller.js
+++ api/Controllers/post.controller.js
@@ -1,4 +1,4 @@
-import { catchAsync } from "vanta-api";
+import ApiFeatures, { catchAsync } from "vanta-api";
 import Post from "../Models/post.model.js";
 
 export const create = catchAsync(async (re, res, next) => {
@@ -7,5 +7,20 @@ export const create = catchAsync(async (re, res, next) => {
         success: true,
         data: post,
         message: 'create post successfully'
+    })
+})
+export const getAll = catchAsync(async(req,res,next)=>{
+    const features = new ApiFeatures(Post,req.query,req.role)
+    .filter()
+    .sort()
+    .paginate()
+    .limitFields()
+    .populate()
+    const posts = await features.execute()
+    const count = await Post.countDocuments(features.queryFilter)
+    return res.status(200).json({
+        success:true,
+        data:posts,
+        count
     })
 })
